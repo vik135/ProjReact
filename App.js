eddbcb36a94fb14b5a376cf467f5d1155b6cd31c
vik135 <65745406+vik135@users.noreply.github.com>
2020-06-03 12:58:21 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
+import SignUp from './src/screens/RegisterScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+import AboutScreen from './src/screens/AboutScreen';
+import DestinationScreen from './src/screens/DestinationScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,7 +14,12 @@ function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Destination" component={DestinationScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={SignUp} />
+        <Stack.Screen name="Review" component={ReviewScreen} />
+        <Stack.Screen name="AboutUs" component={AboutScreen} />
+       
         </Stack.Navigator>
     </NavigationContainer>
   );
